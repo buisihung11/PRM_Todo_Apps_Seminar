93f8e2e384fb02140546f9feda9c46bf97c7f81b
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-07-06 12:49:52 +0700

    Implement Detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:TodoApp_Seminar_PRM/all.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,11 +16,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        body: Center(
-          child: Text("Todo Apps"),
-        ),
-      ),
+      home:DetailScreen(),
+      
     );
   }
 }
